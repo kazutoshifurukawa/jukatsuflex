0ab9d0a31baae7e085a33b9ddcb1f489d6bc0fa8
Kazuki Imamura <kaizumaki@gmail.com>
2023-08-02 18:17:28 +0900

    srcディレクトリを設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  srcDir: 'src/',
   modules: [
     '@nuxthq/ui',
     '@nuxtjs/eslint-module',
   ],
   typescript: {
-    shim: true,
+    shim: false,
     strict: true,
-    typeCheck: true,
+    typeCheck: false,
   },
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    <h1>
+      test
+    </h1>
+  </div>
+</template>
