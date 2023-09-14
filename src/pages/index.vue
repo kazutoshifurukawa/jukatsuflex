2d458c2e13de5ec0e62d999dc1b7b77cf1f2eede
Kazuki Imamura <kaizumaki@gmail.com>
2023-09-14 16:13:23 +0900

    estimateのAPI呼び出しを仮実装

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
-const { data } = await useFetch('/api/hello')
+const config = useRuntimeConfig()
+const estimateItem: Ref<any> = ref(null)
+
+async function getEstimate() {
+  const { data, error } = await useFetch<any>(
+    `${config.public.API_ENDPOINT}/estimate`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        fixpart: 'door,floor',
+      }),
+    },
+  )
+  if (error.value) {
+    // eslint-disable-next-line no-console
+    console.error(error.value)
+  } else {
+    estimateItem.value = data.value
+  }
+}
 </script>
 
 <template>
   <div>
-    <h1>
-      {{ data?.hello }}
-    </h1>
+    <button @click="getEstimate">Get Estimate</button>
+    <pre>{{ estimateItem }}</pre>
   </div>
 </template>
